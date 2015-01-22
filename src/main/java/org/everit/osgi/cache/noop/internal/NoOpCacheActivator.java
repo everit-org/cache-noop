bca93d1e45245e4e05d0dc258e913a49139cc260
attila.kiss <attila.kiss@everit.biz>
2015-01-22 11:43:17 +0100

    generics added to the activator

--- src/main/java/org/everit/osgi/cache/noop/internal/NoOpCacheActivator.java
+++ src/main/java/org/everit/osgi/cache/noop/internal/NoOpCacheActivator.java
@@ -25,7 +25,7 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-public class NoOpCacheActivator implements BundleActivator {
+public class NoOpCacheActivator<K, V> implements BundleActivator {
 
     private ServiceRegistration<?> cacheSR;
 
@@ -35,7 +35,7 @@ public class NoOpCacheActivator implements BundleActivator {
         properties.put("cacheName", "noop");
         cacheSR = context.registerService(
                 new String[] { ConcurrentMap.class.getName(), Map.class.getName() },
-                new NoOpConcurrentMap(),
+                new NoOpConcurrentMap<K, V>(),
                 properties);
     }
 
