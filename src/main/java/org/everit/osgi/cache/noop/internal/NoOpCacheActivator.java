13d0147358310c41d59309244471c4fd6e0e2fb8
attila.kiss <attila.kiss@everit.biz>
2015-02-10 10:13:55 +0100

    implementing cache-api 3.0.0

--- src/main/java/org/everit/osgi/cache/noop/internal/NoOpCacheActivator.java
+++ src/main/java/org/everit/osgi/cache/noop/internal/NoOpCacheActivator.java
@@ -18,27 +18,25 @@ package org.everit.osgi.cache.noop.internal;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
-import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
 
+import org.everit.osgi.cache.CacheConstants;
+import org.everit.osgi.cache.noop.NoOpCacheConstants;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 public class NoOpCacheActivator<K, V> implements BundleActivator {
 
-    private static final String PROP_CACHE_DRIVER_NAME = "cache.driver.name";
-
-    private static final String DEFAULT_CACHE_DRIVER_NAME = "noop";
-
     private ServiceRegistration<?> cacheSR;
 
     @Override
     public void start(final BundleContext context) throws Exception {
         Dictionary<String, String> properties = new Hashtable<String, String>(1);
-        properties.put(PROP_CACHE_DRIVER_NAME, DEFAULT_CACHE_DRIVER_NAME);
+        properties.put(CacheConstants.PROP_CACHE_DRIVER_NAME, NoOpCacheConstants.CACHE_DRIVER_NAME);
         cacheSR = context.registerService(
-                new String[] { ConcurrentMap.class.getName(), Map.class.getName() },
+                new String[] {
+                        CacheConstants.OBJECT_CLASS_NAME_CONCURRENT_MAP,
+                        CacheConstants.OBJECT_CLASS_NAME_MAP },
                 new NoOpConcurrentMap<K, V>(),
                 properties);
     }
